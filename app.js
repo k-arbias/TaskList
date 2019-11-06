5e7f90d483ca2c055c9f4808dade7413f8c922e3
Arbias <k.arbias@live.com>
2019-11-06 20:37:55 +0100

    Added functions to Add, Remove, Filter and Clear Tasks

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ loadEventListeners();
 
 function loadEventListeners(){
     form.addEventListener('submit', addTask);
+    taskList.addEventListener('click', removeTask);
+    clearBtn.addEventListener('click', clearTasks);
+    filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e){
@@ -23,4 +26,32 @@ function addTask(e){
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are You Sure?')){
+        e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+function clearTasks(){
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        }else{
+            task.style.display = 'none';
+        }
+    });
+
 }
